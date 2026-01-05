f19777e7fd872cb656ba0496e29f200c5e8d144d
Daniel Dantas <contatodanieldantasdev@gmail.com>
2026-01-04 23:03:07 -0300

    feat: adição do arquivo app-routing.module.ts

---
+++ developments/DevFreelaAngular-15/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

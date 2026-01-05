f636247b4259f3f9345de377d1070f269b37d827
Daniel Dantas <contatodanieldantasdev@gmail.com>
2026-01-04 23:03:14 -0300

    feat: adição do arquivo ld-wrapper.component.ts

---
+++ developments/DevFreelaAngular-15/src/app/features/ld-wrapper/ld-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ld-wrapper',
+  templateUrl: './ld-wrapper.component.html',
+  styleUrls: ['./ld-wrapper.component.scss']
+})
+export class LdWrapperComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
